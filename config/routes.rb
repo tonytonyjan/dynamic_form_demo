a5996dd0d38f701f62632834e98e8bf154f3c729
Tony Jian <tonytonyjan@gmail.com>
2014-05-06 23:11:04 +0800

    prepare assets, set locale

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    resources :attendees
+  end
+
+  namespace :admin do
+    resources :forms
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
